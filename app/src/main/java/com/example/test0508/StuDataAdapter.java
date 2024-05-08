068cf25ddfd2e1506a79869932eead3c61b385d8
cflin <cflin@mail.cjcu.edu.tw>
2024-05-08 15:56:00 +0800

    完成 recyclerview

---
+++ app/src/main/java/com/example/test0508/StuDataAdapter.java
@@ -0,0 +1,52 @@
+package com.example.test0508;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class StuDataAdapter extends RecyclerView.Adapter<StuDataAdapter.ViewHolder> {
+
+    private TextView tvName1;
+    private TextView tvHeight1;
+    private List<StuData> stuDataList;
+
+    public StuDataAdapter(List<StuData> stuDataList) {
+        this.stuDataList = stuDataList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        View view = View.inflate(context, R.layout.activity_my_data_view, null);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        StuData stuData = stuDataList.get(position);
+        tvHeight1.setText(stuData.getHeight());
+        tvName1.setText(stuData.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return stuDataList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tvName1 = itemView.findViewById(R.id.tvname);
+            tvHeight1 = itemView.findViewById(R.id.tvHeight);
+        }
+    }
+}
