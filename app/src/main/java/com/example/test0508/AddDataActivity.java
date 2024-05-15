4aaf2211ba81ace35caf3f3be258dd7f66694ccb
kid35648466 <111b13982@mailst.cjcu.edu.tw>
2024-05-15 16:11:36 +0800

    未完成 ver1.1

--- app/src/main/java/com/example/test0508/AddDataActivity.java
+++ app/src/main/java/com/example/test0508/AddDataActivity.java
@@ -1,19 +1,34 @@
- package com.example.test0508;
+package com.example.test0508;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
- public class AddDataActivity extends AppCompatActivity {
+public class AddDataActivity extends AppCompatActivity {
+
+    private TextView addName;
+    private TextView addHeight;
+    private TextView addUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_data);
 
-        TextView tvName = findViewById(R.id.edAddName);
-        TextView tvHeight = findViewById(R.id.edAddHeight);
-        TextView tvImageUrl = findViewById(R.id.edAddImageUrl);
+        addName = findViewById(R.id.edAddName);
+        addHeight = findViewById(R.id.edAddHeight);
+        addUrl = findViewById(R.id.edAddImgURL);
+
+    }
+
+    public void goBack(View view) {
+        Intent intent = getIntent();
+        String name = addName.getText().toString();
+        String height = addHeight.getText().toString();
+        String url = addUrl.getText().toString();
+
     }
 }
--- app/src/main/java/com/example/test0508/MainActivity.java
+++ app/src/main/java/com/example/test0508/MainActivity.java
@@ -1,22 +1,31 @@
 package com.example.test0508;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private List<StuData> stuDataList;
+    private StuDataAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = findViewById(R.id.rvMyData);
+        recyclerView = findViewById(R.id.rvMyData);
+
+        stuDataList = new ArrayList<>();
 
         List<StuData> stuDataList = new ArrayList<>();
         stuDataList.add(new StuData("https://png.pngtree.com/png-vector/20221222/ourmid/pngtree-super-cute-cartoon-vector-bear-png-image_6504049.png", "John", "180"));
@@ -29,8 +38,27 @@ public class MainActivity extends AppCompatActivity {
         stuDataList.add(new StuData(R.drawable.logo, "Lucy", "145"));
         stuDataList.add(new StuData(R.drawable.logo, "Linda", "140"));
         stuDataList.add(new StuData(R.drawable.logo, "Marry", "135"));
-        recyclerView.setAdapter(new StuDataAdapter(stuDataList));
+        adapter = new StuDataAdapter(stuDataList);
+        recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+    }
+
+    public void addData(View view) {
+        Intent intent = new Intent(this, AddDataActivity.class);
+        startActivityForResult(intent, 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String name = data.getStringExtra("name");
+            String height = data.getStringExtra("height");
+            String url = data.getStringExtra("url");
+            stuDataList.add(new StuData(url, name, height));
+            adapter.notifyDataSetChanged();
+        }
     }
 }
